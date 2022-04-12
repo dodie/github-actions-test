413630c1e2313be738d6531b3d8e07bfa1c9e324
Dávid Csákvári <david.csakvari@adnovum.hu>
2022-04-12 12:30:36 +0200

    Add app

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,17 @@
+/*
+ * Author : AdNovum Informatik AG
+ */
+
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+	@GetMapping("/")
+	public String hello() {
+		return "index";
+	}
+}
